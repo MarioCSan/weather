9af78a2e8a546f63569ffaece8888327ba64b22d
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-12 15:32:48 +0200

    Se realizan cambios en las llamadas, se modifica la url y la apikey. Además se empieza a montar la llamada a la API

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import Global from '../Global';
 import './App.css';
 
+
 function App() {
+  this.url = Global.weather
+  this.key = Global.key
+  let url = this.url + this.key
+
+  useEffect(()=>{
+    fetch(url)
+  })
   return (
     <div className="App">
-      
+      <h1> Weather </h1>
     </div>
   );
 }
--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,6 @@
 let Global = {
-    'weather': 'http://api.weatherapi.com/v1',
-    'APiKey': ''
+    'weather': 'http://api.weatherapi.com/v1/current.json?key=',
+    'APiKey': '0cad98a1b2c448b794b93715230306'
 }
 
 export default Global;
