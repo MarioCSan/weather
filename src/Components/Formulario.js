01153c0c6f56cdc77ac37307ba4db970d39447ff
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-19 19:57:19 +0200

    Se renombra Tiempo a Formulario, puesto que es dode se recoje la ciudad. Se crea el componente Tiempo que sera donde se reciban los datos mediante props

---
+++ src/Components/Formulario.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import Global from "../Global";
+import axios from "axios";
+
+
+const Formulario = ()=>{
+    const [ciudad, setCiudad] = useState('')
+    const [data, setData] = useState([]);
+    const url = Global.url 
+    let city = '?q=madrid&lang=es&key='
+    const key = Global.ApiKey
+  
+    async function fetchData() {
+        axios.get(url+city+key).then((res) => {
+            setData(res.data);
+          });
+      }
+
+
+  
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if([ciudad].includes('')){
+            // ! Error
+        } else {
+           city = city.replace('madrid', ciudad)
+           fetchData()
+           console.log(data)
+        }
+
+    }
+ 
+    const handleChange = (e) =>{
+        e.preventDefault();
+
+        if([ciudad].includes('')){
+            // ! Error
+        } else {
+           fetchData()
+        }
+       
+    }
+
+    useEffect(()=>{
+        fetchData() 
+    })
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="ciudad">Ciudad</label>
+                    <input id='ciudad' placeholder="Ciudad" value={ciudad} onChange={ (e) => setCiudad(e.target.value) } onBlur={handleChange}></input>
+                </div>
+                <input
+                    type="submit"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                />
+            </form> 
+            
+            
+        </div>
+    )
+}
+
+export default Formulario
