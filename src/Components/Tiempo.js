ee2cc601a9fee95ab021fc678499d1af9e96471b
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-20 19:48:23 +0200

    se comienzan a recuperar todos los datos de la api y mostrarlos de manera rudimentaria para comprobar su funcionamiento

--- src/Components/Tiempo.js
+++ src/Components/Tiempo.js
@@ -4,16 +4,32 @@ const Tiempo = ({data})=>{
      return(
         <div>
             {data.location !== undefined && (
-                <div className="p-10 m-10">
-                    <div className="flex place-content-center">
-                         <img src={data.current.condition.icon}/>                   
+               <div>
+                    <div className="p-10 m-10">
+                        <div className="flex place-content-center">
+                            <img src={data.current.condition.icon}/>                   
+                        </div>
+                    
+                        <h3>{data.location.name}</h3>
+                        <h3>{data.current.condition.text}</h3>
+                        <h4>{data.current.temp_c}</h4>
+                        </div>
+                    <div>
+                        {data.forecast.forecastday.map((item)=>{
+                            return(
+                                <div>
+                                    <h3>
+                                        {item.day.maxtemp_c}
+                                    </h3>
+                                    <h3>
+                                        {item.date}
+                                    </h3>
+                                </div>
+                            )
+                        })}
                     </div>
-                   
-                    <h3>{data.location.name}</h3>
-                    <h3>{data.current.condition.text}</h3>
-                    <h4>{data.current.temp_c}</h4>
-                </div>
-             )}   
+                </div> 
+            )}   
            
         </div>
     )
