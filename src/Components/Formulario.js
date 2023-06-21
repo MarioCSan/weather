305d222d4f5f9b890fc12e7b1d2ca163f5d43ac0
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-21 15:29:59 +0200

    Se modifica el input para que el texto este centrado repecto al input

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -57,8 +57,8 @@ const Formulario = () =>{
                     <div className="flex place-content-center">
                    
                         <input id='ciudad' placeholder="Ciudad" 
-                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-1/2 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm 
-                                            focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-smv items-center"
+                                className="placeholder:italic md:text-center placeholder:text-slate-400 block bg-white w-1/2 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm 
+                                            focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-smv"
                                 value={ciudad} onChange={ (e) => setCiudad(e.target.value) } onBlur={handleSubmit}></input>
                     </div>
                     <input
