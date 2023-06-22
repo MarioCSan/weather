572950f103ee6e4b02f724a56bed2d687c5b538c
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-22 20:53:39 +0200

    Cambios en la llamada para https

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,6 @@
 let Global = {
     'current': 'https://api.weatherapi.com/v1/current.json?q=madrid&lang=es&key=0cad98a1b2c448b794b93715230306',
-    'forecast': 'http://api.weatherapi.com/v1/forecast.json?key=0cad98a1b2c448b794b93715230306 &q=',
+    'forecast': 'https://api.weatherapi.com/v1/forecast.json?key=0cad98a1b2c448b794b93715230306 &q=',
     'url': 'https://api.weatherapi.com/v1/current.json?q=',
     'ApiKey': '0cad98a1b2c448b794b93715230306',
 }
