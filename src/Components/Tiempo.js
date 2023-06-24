c59de616a3834a6f3760951612796b1a909a6c66
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-06-24 20:44:01 +0200

    Se solucionan algunos errores que afectan a las imagenes

--- src/Components/Tiempo.js
+++ src/Components/Tiempo.js
@@ -16,14 +16,14 @@ const Tiempo = ({ data }) => {
               Previsión del tiempo en {data.location.name}
             </h1>
             <div className="flex place-content-center">
-              <img src={data.current.condition.icon} alt="icon" />
+              <img src={data.current.condition.icon} alt="icon" width="64" height="64"/>
             </div>
 
-            <h3 className="font-bold">{data.current.condition.text}</h3>
-            <h4>{data.current.temp_c} ºC</h4>
+            <h2 className="font-bold">{data.current.condition.text}</h2>
+            <h3>{data.current.temp_c} ºC</h3>
             <h3>{data.location.localtime}</h3>
-            <h4>Sensación térmica: {data.current.feelslike_c} ºC</h4>
-            <h4>Viento: {data.current.wind_kph} Km/h</h4>
+            <h3>Sensación térmica: {data.current.feelslike_c} ºC</h3>
+            <h3>Viento: {data.current.wind_kph} Km/h</h3>
           </div>
           <div className="">
             <h3 className="text-2xl Font-bold"> Próximos días</h3>
@@ -35,7 +35,7 @@ const Tiempo = ({ data }) => {
                     className="rounded overflow-hidden shadow-lg p-12 m-6 bg-sky-400"
                   >
                     <div className="flex place-content-center">
-                      <img src={item.day.condition.icon} alt="icon" />
+                      <img src={item.day.condition.icon} alt="icon" width="64" height="64"/>
                     </div>
                     <div className="pt-4">
                       <h3 className="font-bold">{item.day.condition.text}</h3>
